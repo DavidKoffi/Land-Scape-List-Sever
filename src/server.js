4e79304d75a50fccb4a3c0860b3ece9a21df9f8e
David Koffi <davidnkoffi@Davids-Mac-mini.local>
2023-03-15 22:35:56 -0400

    fixed body parse

--- src/server.js
+++ src/server.js
@@ -5,10 +5,14 @@ const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 const { SendEmail } = require("./emailService.js");
+
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const certificate = fs.readFileSync("../A9D236016B3303B761D3E70A385DB4BD.txt");
 
@@ -28,6 +32,9 @@ const corsOptions = {
 };
 
 app.use(cors());
+
+app.use(jsonParser);
+app.use(urlencodedParser);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -72,43 +79,39 @@ app.get(
   }
 );
 
-app.post(
-  "/",
-  upload.array("image" /* name attribute of <file> element in your form */),
-  (req, res) => {
-    // const arrOfName = [];
-    // for (let i = 0; i < req.files?.length; i++) {
-    //   uploadImage(req, res, i, arrOfName);
-    // }
+app.post("/", jsonParser, (req, res) => {
+  // const arrOfName = [];
+  // for (let i = 0; i < req.files?.length; i++) {
+  //   uploadImage(req, res, i, arrOfName);
+  // }
 
-    // let attachments = [];
-    // arrOfName.forEach((value) => {
-    //   attachments.push({ path: value });
-    // });
-    let data = req.body;
-    console.log(req.body);
+  // let attachments = [];
+  // arrOfName.forEach((value) => {
+  //   attachments.push({ path: value });
+  // });
+  let data = req.body;
+  console.log(req.body);
 
-    SendEmail(
-      "seanyousefi5@gmail.com",
-      "New Job",
-      `Client name: ${data.firstName} ${data.lastName} \n 
-        Client address: ${data.address} \n
-        Client zip: ${data.zipCode} \n
-        Client phone number: ${data.phoneNumber} \n
-        Client email: ${data.email} \n
-        Expected completion date: ______ \n
-        Job type: ${data.service} \n
-        additional info: ${data.additionalInfo} \n
-        Out Reach type: ${data.outReachType}`
-      //attachments
-    );
-    if (res.statusCode == 200) {
-      console.log("success");
-      res.status(200).json({ success: true });
-    }
-    // res.status(200).contentType("text/plain").end("File uploaded!");
+  SendEmail(
+    "seanyousefi5@gmail.com",
+    "New Job",
+    `Client name: ${data.firstName} ${data.lastName} \n
+       Client address: ${data.address} \n
+       Client zip: ${data.zipCode} \n
+       Client phone number: ${data.phoneNumber} \n
+       Client email: ${data.email} \n
+       Expected completion date: ______ \n
+       Job type: ${data.service} \n
+       additional info: ${data.additionalInfo} \n
+       Out Reach type: ${data.outReachType}`
+    //attachments
+  );
+  if (res.statusCode == 200) {
+    console.log("success");
+    res.status(200).json({ success: true });
   }
-);
+  // res.status(200).contentType("text/plain").end("File uploaded!");
+});
 
 function uploadImage(req, res, num, arrayOfNames) {
   //get the path temp of the file uploaded from the form
